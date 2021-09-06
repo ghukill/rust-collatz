862be33c89678ee50ca2dda3b7b6eeb703d299be
Graham Hukill <ghukill@gmail.com>
2021-09-06 15:49:34 +0200

    now with graphs!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use std::env;
-
+use terminal_size::{Width, Height, terminal_size};
+use textplots::{Chart, Plot, Shape};
 
 
 fn main() {
@@ -10,17 +11,18 @@ fn main() {
 
     // get input number
     let mut i: u32 = args[1].parse::<u32>().unwrap();
-
-    println!("Your input number is {}, now let's determine the Collatz conjecture altitude + steps", i);
+    let seed_value = i.clone();
 
     // bookkeeping
     let mut max_altitude: u32 = 1;
-    let mut step_count: u32 = 0;
+    let mut step_count: u32 = 1;
+    let mut points: Vec<(f32, f32)> = Vec::new();
+    points.push((step_count as f32, i as f32));
 
     // loop until i == 1 (NOTE: because a reference, this check works)
     while i > 1 {
 
-        println!("working on: {}...",i);
+        // println!("working on: {}...",i);
 
         // bump step count
         step_count += 1;
@@ -37,8 +39,23 @@ fn main() {
         if i > max_altitude {
             max_altitude = i;
         }
+
+        // push to altitudes vector
+        points.push((step_count as f32, i as f32));
     }
 
-    println!("Finis!  Max altitude: {}, step count: {}.", max_altitude, step_count)
+    println!("Seed value: {}, max altitude: {}, step count: {}.  Now buckle up for a pretty graph:", seed_value, max_altitude, step_count);
+
+    // graph
+    let size = terminal_size();
+    if let Some((Width(w), Height(h))) = size {
+        Chart::new((w as f32 *1.5 ) as u32, (h as f32*1.5) as u32, 1 as f32, step_count as f32)
+            .lineplot(&Shape::Lines(&points)).display();
+    } else {
+        println!("Unable to get terminal size, using some defaults...");
+        Chart::new(200, 50, 1 as f32, step_count as f32)
+            .lineplot(&Shape::Lines(&points)).display();
+    }
+
 
 }
